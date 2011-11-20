21a9258e760592fc9bea16e6314cfa7e3a8028ce
molamil <molamil@2b527605-c68c-3f2b-6faf-6e293b1535fe>
2011-11-20 17:40:36 +0000

    Example of transitions on Molatester

--- molatester/scripts/mola.js
+++ molatester/scripts/mola.js
@@ -1,13 +1,81 @@
 (function(window) {
 
 
-	// *** Mola OBJECT
+	// *** STATE MANAGERS
+
+	var StateManagers = {
+
+		init: function() {
+
+
+			// *** CASCADE CLASS
+
+			var Cascade = function(target, session) {
+				Zumo.StateManagers.BaseIo3Manager.call(this, target, session);
+				this.inDelay = 300;
+				this.delay = 90;
+				this.timeIn = 200;
+				this.timeOut = 200;
+			};
+
+			Cascade.prototype = {
+
+				_doIn: function() {
+					var that = this;
+					var $div = $("div", this.target);
+					for (var i = 0; i < $div.length; i++) {
+						var $thisDiv = $($div[i]);
+						var callback;
+						if (i == $div.length - 1) {
+							callback = function() {
+								that.setState(Zumo.StateManagers.STATE_ON);
+							}
+						}
+						$thisDiv.css("display", "none");
+						$thisDiv.delay(i * that.delay + that.inDelay).fadeIn(that.timeIn, callback);
+					}
+				},
+
+				_doOut: function() {
+					var that = this;
+					var $div = $("div", this.target);
+					for (var i = 0; i < $div.length; i++) {
+						var $thisDiv = $($div[i]);
+						var callback;
+						if (i == $div.length - 1) {
+							callback = function() {
+								that.setState(Zumo.StateManagers.STATE_OFF);
+							}
+						}
+						$thisDiv.delay(($div.length - 1 - i) * that.delay).fadeOut(that.timeOut * 2, callback);
+					}
+				}
+
+			};
+
+
+			// *** INIT
+
+			Zumo.ObjectUtils.extend(Cascade, Zumo.StateManagers.BaseIo3Manager);
+			Zumo.registerStateManager("_cascade", Cascade);
+
+
+		}
+
+	}
+
+
+	// *** MOLA OBJECT
 
 	var Mola = {
 
 		init: function() {
 
 			this.initMenu();
+
+			StateManagers.init();
+
+			$("#header").fadeIn("fast");
 
 			// Rollovers
 
@@ -25,13 +93,14 @@
 			var pageContexts = Zumo.getPageContexts();
 			for (var i = 0; i < pageContexts.length; i++) {
 				var pageContext = pageContexts[i];
-				var li = $(document.createElement("li"));
-				li.text(pageContext.title);
-				li.click({id: pageContext.id}, function(event) {
-					window.console.info("*** ID: " + event.data.id);
+				var $li = $(document.createElement("li"));
+				$li.text(pageContext.title);
+				$li.css("display", "none");
+				$li.delay((pageContexts.length - i) * 200).fadeIn("slow");
+				$li.click({id: pageContext.id}, function(event) {
 					Zumo.goto(event.data.id);
 				});
-				var menuItem = $("#menu").append(li);
+				$("#menu").append($li);
 			}
 		}
 
@@ -51,7 +120,7 @@
 
 $(function() {
 
-	Zumo.log.level = 3;
+	Zumo.log.level = 2;
 
 	Zumo.init(window.document, "zumo.xml");
 
