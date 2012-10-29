80a6752ec7524ef71702cf389942140fbee51a79
molamil@gmail.com <molamil@gmail.com@2b527605-c68c-3f2b-6faf-6e293b1535fe>
2012-10-29 19:17:41 +0000

    Split log in its own source file

---
+++ src/log.js
@@ -0,0 +1,54 @@
+
+
+    // *** LOG - OBJECT
+
+    var Log = {
+
+        // --- PROPERTIES
+
+        LEVELS: ["error", "warn", "info", "debug"],
+        level: 1,
+        prefix: _NAME ? _NAME.toUpperCase() + " - " : "", //TODO: Set prefix elsewhere.
+
+        // --- METHODS
+
+        debug: function(message) {
+            if (typeof message == "string")
+                message = this.prefix + message;
+            this._log(message, 3);
+        },
+
+        info: function(message) {
+            this._log(this.prefix + message, 2);
+        },
+
+        warn: function(message) {
+            this._log(this.prefix + message, 1);
+        },
+
+        error: function(message) {
+            this._log(this.prefix + message, 0);
+        },
+
+        // Default Firebug console logging implemented.
+        _log: function(message, level) {
+
+            var fLevel;
+
+            // Check that Firebug is enabled.
+            if (!window.console)
+                return;
+
+            // Set level as default if not passed.
+            if (level == null)
+                level = this.level;
+
+            if (this.level >= level) {
+                fLevel = window.console[this.LEVELS[level]];
+                if (typeof fLevel == "function")
+                    fLevel.call(window.console, message);
+            }
+
+        }
+
+    };
